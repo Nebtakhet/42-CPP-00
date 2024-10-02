99a3c5bee57dfc0f3007420b710cd8e759170741
Cesar Sanchez <cesasanc@c1r6p16.hive.fi>
2024-10-02 14:56:58 +0300

    Added ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cesasanc <cesasanc@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/02 13:13:23 by cesasanc          #+#    #+#             */
+/*   Updated: 2024/10/02 14:13:04 by cesasanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				argv[i][j] = (char)toupper(argv[i][j]);
+			std::cout << argv[i];
+			if (argv[i])
+				std::cout << " ";
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
