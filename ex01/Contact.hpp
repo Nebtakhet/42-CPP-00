663878c14cbd99481fa551ecd36175c59adb5a5a
Cesar Sanchez <cesasanc@c1r6p16.hive.fi>
2024-10-02 14:57:19 +0300

    Added ex01, not finished

---
+++ ex01/Contact.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cesasanc <cesasanc@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/02 14:13:39 by cesasanc          #+#    #+#             */
+/*   Updated: 2024/10/02 14:25:31 by cesasanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <iostream>
+# include <string>
+
+class Contact
+{
+	
+}
+
+#endif
